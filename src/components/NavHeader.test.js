2b52f4ec78c81e0ab41dd89f453ca3471db6854c
Lewis Sellers <lasellers@gmail.com>
2020-08-30 17:28:38 -0400

    Add error handling ui

---
+++ src/components/NavHeader.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import { render } from '@testing-library/react';
+import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
+
+var chai = require('chai');
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style to.be ...
+var should = chai.should();  // Using Should style
+import ErrorBox from "./NavHeader";
+
+it("renders without crashing", () => {
+    shallow(<NavHeader/>);
+});
+/*
+it("renders", () => {
+    const wrapper = shallow(<ErrorBox/>);
+    const title = <h1>CSV File Upload</h1>
+    expect(wrapper.contains(title)).to.equal(true);
+});
+*/
