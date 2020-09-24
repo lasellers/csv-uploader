b03b1b2fd9524c59f2b4d017c3cbc067a963e037
Lewis Sellers <lasellers@gmail.com>
2020-09-24 13:19:49 -0400

    Lint fix

--- vuespa/src/store/store.js
+++ vuespa/src/store/store.js
@@ -46,52 +46,52 @@ export default new Vuex.Store({
     },
 
     mutations: {
-        addCsvHeaders(store, value) {
+        addCsvHeaders (store, value) {
             store.csvHeaders = value
         },
-        addCsvData(store, value) {
+        addCsvData (store, value) {
             store.csvData = value
         },
-        addCustomAttributes(store, value) {
+        addCustomAttributes (store, value) {
             store.customAttributes = value
         },
-        addContacts(store, value) {
+        addContacts (store, value) {
             store.contacts = value
         },
-        addRemappedColumnOrder(store, value) {
+        addRemappedColumnOrder (store, value) {
             store.remappedColumnOrder = value
         },
-        addErrors(store, errors) {
+        addErrors (store, errors) {
             store.errors = errors
         },
-        clearErrors(store) {
+        clearErrors (store) {
             store.errors = null
         }
     },
 
     actions: {
-        addCsvHeaders(context, value) {
+        addCsvHeaders (context, value) {
             context.commit('addCsvHeaders', value)
         },
-        addCsvData(context, value) {
+        addCsvData (context, value) {
             context.commit('addCsvData', value)
         },
-        addCustomAttributes(context, value) {
+        addCustomAttributes (context, value) {
             context.commit('addCustomAttributes', value)
         },
-        addContacts(context, value) {
+        addContacts (context, value) {
             context.commit('addContacts', value)
         },
-        addRemappedColumnOrder(context, value) {
+        addRemappedColumnOrder (context, value) {
             context.commit('addRemappedColumnOrder', value)
         },
-        addErrors(context, values) {
+        addErrors (context, values) {
             if (values === null || values === '' || values === [] || values === {} ||
                 (Array.isArray(values) && values.length === 0)) {
                 // do nothing
                 context.commit('addErrors', null)
             } else if (typeof values === 'string') {
-                values = [{field: 'unknown', message: values, key: 0}]
+                values = [{ field: 'unknown', message: values, key: 0 }]
                 context.commit('addErrors', values)
             } else {
                 values = [...values]
@@ -104,14 +104,14 @@ export default new Vuex.Store({
                 const messages = []
                 for (const [key, value] of Object.entries(errors)) {
                     for (const [field, message] of Object.entries(value)) {
-                        messages.push({field, message, key})
+                        messages.push({ field, message, key })
                     }
                 }
 
                 context.commit('addErrors', messages)
             }
         },
-        clearErrors(context) {
+        clearErrors (context) {
             context.commit('clearErrors')
         }
     }
